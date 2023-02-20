74d5ebba1e8996e7693402cfb88150aed6f8ce93
Suntra <qawseswa1931@gmail.com>
2023-02-20 21:46:22 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -118,58 +118,11 @@ let page = {
                 throw new Error("ERROR: please format your input");
             }
 
-            let bird = page.bird = new DrunkenBird(w, homePos, allowNegativePos);
+            page.bird = new DrunkenBird(w, homePos, allowNegativePos);
 
-            bird.randomMove(times);
+            page.bird.randomMove(times);
 
-            let option = {
-                title: {
-                    text: "喝醉的小鸟2d展示"
-                },
-                tooltip: {
-                    trigger: "item"
-                },
-                xAxis: {
-                    name: "x"
-                },
-                yAxis: {
-                    name: "y"
-                },
-                series: []
-            };
-
-            option.series.push({
-                type: "scatter",
-                symbol: "path://M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z",
-                symbolSize: 16,
-                tooltip: {
-                    formatter: function ({ data }) {
-                        console.log(arguments);
-                        return `小鸟:<br />x:${data[0]} y:${data[1]}`;
-                    }
-                },
-                data: [
-                    [bird.birdPos[0], bird.birdPos[1]]
-                ],
-                clip: false
-            }, {
-                type: "scatter",
-                symbol: "path://M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207l-5-5-5 5V13.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7.207Z",
-                symbolSize: 16,
-                tooltip: {
-                    formatter: function ({ data }) {
-                        console.log(arguments);
-                        return `家:<br />x:${data[0]} y:${data[1]}`;
-                    }
-                },
-                data: [
-                    [bird.homePos[0], bird.homePos[1]]
-                ],
-                clip: false
-            });
-
-            // 绘制图表
-            page.chart.setOption(option);
+            page.showChart(page.bird.birdPos, page.bird.homePos);
 
             page.showList();
         } catch (error) {
@@ -177,6 +130,53 @@ let page = {
         } finally {
 
         };
+    },
+    showChart(birdPos, homePos) {
+        // 绘制图表
+        page.chart.setOption({
+            title: {
+                text: "喝醉的小鸟2d展示"
+            },
+            tooltip: {
+                trigger: "item"
+            },
+            xAxis: {
+                name: "x"
+            },
+            yAxis: {
+                name: "y"
+            },
+            series: [
+                {
+                    type: "scatter",
+                    symbol: "path://M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z",
+                    symbolSize: 32,
+                    tooltip: {
+                        formatter: function ({ data }) {
+                            return `小鸟:<br />x:${data[0]} y:${data[1]}`;
+                        }
+                    },
+                    data: [
+                        [birdPos[0], birdPos[1]]
+                    ],
+                    clip: false
+                },
+                {
+                    type: "scatter",
+                    symbol: "path://M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207l-5-5-5 5V13.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7.207Z",
+                    symbolSize: 32,
+                    tooltip: {
+                        formatter: function ({ data }) {
+                            return `家:<br />x:${data[0]} y:${data[1]}`;
+                        }
+                    },
+                    data: [
+                        [homePos[0], homePos[1]]
+                    ],
+                    clip: false
+                }
+            ]
+        });
     },
     showList() {
         page.table.destroy();
